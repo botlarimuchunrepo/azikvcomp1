81b8805704f2721ea3adfcfba88d56853bec17f1
Abduazim <100228123+azikdeveloper017@users.noreply.github.com>
2022-04-17 20:22:50 +0500

    Add files via upload

---
+++ LOCAL/localisation.py
@@ -0,0 +1,52 @@
+#Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
+#Respect my work by not making any changes here.
+
+START_TEXT = """Salom🖖 Mendan foydalanib siz⤵️ 
+
+Video hajmini siqishingiz mumkin🗜✅
+
+Boshqa jarayon davom etayotgan bo'lsa boshqa botlarimizda sinab ko'ring👇
+
+👉 @azik_compressbot @azik_compress2bot 👈
+
+Video yuboring!!
+@azik_projects - 𝚃𝚘 𝚝𝚑𝚎 𝚏𝚞𝚝𝚞𝚛𝚎 𝚠𝚒𝚝𝚑 𝚞𝚜🦾"""
+
+FORCE_SUB_TEXT = "Ushbu botdan foydalanish uchun siz @azik_cinema kanaliga qo'shilishingiz kerak."
+
+CHANNEL_LINK = "https://t.me/azik_projects"
+
+SUPPORT_LINK = "https://t.me/azik_projects_support"
+
+BOT_LINK = "https://t.me/azik_compress3bot"
+
+info_text = "Bu bot @azik_developer tomonidan ishlab chiqilgan\n\nProyektlarimiz kanali: @azik_projects\nQo'llab Quvvatlash Guruhi : @azik_projects_support\n\n@azik_projects - 𝚃𝚘 𝚝𝚑𝚎 𝚏𝚞𝚝𝚞𝚛𝚎 𝚠𝚒𝚝𝚑 𝚞𝚜🦾"   
+
+aboutDev_text = """@azik_developer tomonidan yaratilgan
+Proyektlar kanali: @azik_projects
+Bizning boshqa proyektlarimizga ham tashrif buyuring👇
+[Fayl Yuklash📥](https://t.me/azik_faylyuklabot)
+[Kino Kanal🎞](https://t.me/azik_cinema)"""
+
+help_text = """**Versiya 1.0.0**
+•`Qayta kodlash🔀` - videongizni turli xil formatiga  kodlang
+•`HEVC Siqish` - Kamroq siqish
+•`TEZ Siqish` - Juda tez va samarali siqish
+•`Konvertatsiya qilish♻️` - Formatlarni o'zgartirish yoki har qanday videoning ovozini chiqarib olish
+•`Qayta nomlash✍️` - Har qanday fayl nomini o'zgartiring, fayl kengaytmasi talab qilinmaydi
+•`Skrenshot olish📸` - Videongizning 10 ta skrinshotini yarating
+•`Kesish✂` - Videoyingizni ma'lum joyini kesish"""
+
+source_text = "**Deploy your own bot**"
+
+DEV = "https://t.me/azik_developer"
+
+spam_notice = "Bu bot heroku-da joylashtirilgan va shuning uchun bir vaqtning o'zida faqat bitta jarayonni bajarishi mumkin. Botni spam qilish yoki kattalar videolarini kodlash sizni botdan ban qilishga olib keladi."
+
+JPG = "LOCAL/video_convertor.jpg"
+
+JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+
+JPG2 = "LOCAL/20211215_165751.jpg"
+
+JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
